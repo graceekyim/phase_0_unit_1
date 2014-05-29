cd0a7137734a381403135a19d4684bdc30a34b2c
Grace Yim <graceekim@gmail.com>
2014-05-28 19:00:28 -0500

    added gc_1 user stories

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,13 @@
+// My role in the group is: Translate tests into user stories (Person 1)
+
+// Here is my part of the challenge:
+
+//Function name: sum
+//I want a program that accepts a list of numbers, calculates the total and gives me the total.
+
+//Function name: mean
+//I want a program that accepts a list of numbers, calculates the average and gives me the average. 
+
+//Function name: median
+//I want a program that accepts a list of numbers and finds the median number. 
+//If the length of the list is an even number, the median should be the average of the two middle numbers.
