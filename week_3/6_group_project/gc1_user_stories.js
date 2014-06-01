10f838c17be320ca386e8d79ac45550e9584b908
Grace Yim <graceekim@gmail.com>
2014-06-01 15:23:15 -0700

    added my reflection to group project

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -11,3 +11,35 @@
 //Function name: median
 //I want a program that accepts a list of numbers and finds the median number. 
 //If the length of the list is an even number, the median should be the average of the two middle numbers.
+
+//Reflection:
+
+// What was this experience like?
+
+// The experience was a little nerve-wracking at first because I wasn't sure of how much detail 
+// to include in the user stories. I was able to ask Maria for some guidance, which helped me get
+// started with the challenge. I also spoke with Jeff to make sure the user stories were clear
+// and that helped alleviate some anxiety I had. I really enjoyed seeing the final code and felt
+// relieved that we were able to pass all the tests. 
+
+// Was your group successful in passing the tests?
+
+// Yes! 
+
+// What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
+
+// I realized that user stories are essential to the process since the information in the user stories 
+// guides every following step. I felt a lot of pressure at first but it was a great learning experience. 
+
+// Prior to writing the user stories, I actually tried writing the final code and that helped me 
+// figure out what details I would need to include in the user stories. It helped me understand why it's
+// important as a developer to understand what the user is trying to do with the program you are
+// building. 
+
+// I feel that I better understand the process of writing user stories, but I'm still not very
+// familiar with writing pseudocode. I'm hoping I'll get more practice with that in future 
+// challenges. 
+
+// I really enjoyed the whole process and it was comforting to know that we were able to complete
+// the project successfully despite my concerns as Person 1 at the beginning of the challenge. 
+
