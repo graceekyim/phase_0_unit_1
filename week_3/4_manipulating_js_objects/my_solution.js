ece35b9f7458dda0ee4efda0903db2b5c9510bd9
Grace Yim <graceekim@gmail.com>
2014-06-02 01:30:05 -0700

    added my cc mission

--- week_3/4_manipulating_js_objects/my_solution.js
+++ week_3/4_manipulating_js_objects/my_solution.js
@@ -17,20 +17,48 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
+// 1. Define a variable adam and use object literal notation to assign this variable the value of a JavaScript Object object with no properties.
+var adam = {};
 
+// 1. Give adam a name property with the value "Adam".
+adam.name="Adam";
 
+// 2. Add a spouse property to terah and assign it the value of adam.
+terah.spouse=adam;
 
+// 3. Change the value of the terah weight property to 125.
+terah.weight=125;
 
+// 4. Remove the eyeColor property from terah.
+delete terah.eyeColor;
+
+// 5. Add a spouse property to adam and assign it the value of terah.
+adam.spouse=terah;
+
+// 6. Add a children property to terah and and use object literal notation to assign this variable the value of a JavaScript Object object with no properties
+terah.children={};
+
+// 7. Add a carson property to the value of the terah children property and assign it the value of an object with the property name with a value of "Carson".
+terah.children.carson={name:"Carson"};
+
+// 8. dd a carter property to the value of the terah children property and assign it the value of an object with the property name with a value of "Carter".
+terah.children.carter={name:"Carter"};
+
+// 9. Add a colton property to the value of the terah children property and assign it the value of an object with the property name with a value of "Colton".
+terah.children.colton={name:"Colton"};
+
+// 10. Add a children property to adam and assign it the value of terah children.
+adam.children=terah.children;
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
 // 
-// 
-// 
-// 
-// 
+// This challenge was pretty easy to follow since I had some practice creating JS objects during other
+// challenges this week. The hardest part was just figuring out the pseudocode and applying it in writing
+// out the code. I'm finding that I'm gradually getting accustomed to the syntax used in JS. I enjoyed
+// most of this challenge and I'm starting to feel more comfortable with using node and Chrome Dev Tools.
 // 
 
 
--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,37 +1,158 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
+// Save Jesse from Uncle Jack by killing him
+// and giving Jesse a chance to escape.
 // 
 
 // Pseudocode
 // 
-// 
-// 
-// 
-// 
+// Create 1 object for the hostage with a name, location and status.
+// Create 1 object for the thug with a name, location and status
+// Create 1 object for the hero with a name, location, status and enemy
+// Create method that moves foward (moveForward)
+// Create method that attacks thug (attack)
+// Create method that moves Jesse (moveHostage)
 
 // Initial Code
 
+var hostage={
+	name: "Jesse",
+	age: 21,
+	location: "jail cell",
+	status: "locked up"
+}
 
+var thug={
+	name: "Uncle Jack",
+	age: 50,
+	location: "at gate",
+	status: "alive"
+}
 
+var hero={
+	name: "Walt",
+	age: 50,
+	location: "outside building",
+	status: "waiting",
+	enemy: "Uncle Jack"
+}
+
+var moveForward = function(){
+	hero.status="ready to attack";
+	alert("It's time to approach the gate and get ready to attack " + hero.enemy + "!");
+}
+
+var attack = function(){
+	if (hero.status==="ready to attack"){
+		alert(hero.name + " attacks and yells \"Die you filthy animal!\"");
+		thug.status="dead"}
+	else {
+		alert(hero.name + " must move forward before attacking.")
+	}
+}
+
+var moveHostage = function(){
+	if (thug.status==="dead"){
+		hostage.location="free";
+		alert(hostage.name + " escapes from the jail cell! Hooray!")
+	}
+	else{
+		alert(hero.name + " must attack before " + hostage.name + " can escape.")
+	}
+
+}
 
 
 
 // Refactored Code
 
+//Create object for hostage
+var hostage={
+	name: "Jesse",
+	location: "jail cell",
+}
 
+//Create object for thug
+var thug={
+	name: "Uncle Jack",
+	location: "at gate",
+}
 
+//Create object for hero
+var hero={
+	name: "Walt",
+	status: "waiting",
+	enemy: "Uncle Jack"
+}
 
+//Create function that moves hero forward
+var moveForward = function(){
+	if (hero.status==="waiting"){
+		hero.status="ready to attack";
+		console.log(hero.name + " has approached the gate and is ready to attack " + hero.enemy + ".")
+	}
+	else if (hero.status==="ready to attack"){
+		console.log(hero.name + " is already outside the building and ready to attack.")
+	}
+	else if (hero.status==="already attacked"){
+		console.log(hero.name + " has already killed " + hero.enemy + ".")
+	}
+	else{
+		console.log(hostage.name + " has already escaped!")
+	}
+}
+
+//Create function that attacks enemy
+var attack = function(){
+	if (hero.status==="ready to attack"){
+		console.log(hero.name + " attacks " + hero.enemy + " and growls, \"I am the danger!\"");
+		thug.status="dead";
+		hero.status="already attacked"
+	}
+	else if (hero.status==="waiting"){
+		console.log(hero.name + " must move forward and attack " + hero.enemy + " before " + hostage.name + " can escape.")
+	}
+	else if(hero.status==="already attacked"){
+		console.log(hero.name + " has already killed " + hero.enemy + ".")
+	}
+	else{
+		console.log(hostage.name + " has already escaped!")
+	}
+}
+
+//Create function that frees hostage
+var freeHostage = function(){
+	if (hero.status==="already attacked"){
+		hero.status="hero";
+		console.log(hostage.name + " escapes from the jail cell! Hooray!")
+	}
+	else if (hero.status==="waiting"){
+		console.log(hero.name + " must move forward and attack " + hero.enemy + " before " + hostage.name + " can escape.")
+	}
+	else if (hero.status==="ready to attack"){
+		console.log(hero.name + " must attack " + hero.enemy + " before " + hostage.name + " can escape.")
+	}
+	else {
+		console.log(hostage.name + " has already escaped!")
+	}
+}
 
 
 // Reflection
 // 
-// 
-// 
-// 
-// 
+// The most difficult part of this challenge was writing the pseudocode. Since I generally like to dive in 
+// head first and start writing the code, I had to fight the temptation to do that this time. I tried
+// my best to focus on the mission description and started from there. 
+
+// After writing the initial code, I ended up making significant changes during the refactoring. I believe 
+// we are supposed to focus on cleaning up the code itself and not necessarily changing the external 
+// behavior. Nonetheles, I found myself adding several if and else if statements to make the code more 
+// usable and comprehensive. 
+
+// Overall, I feel like this challenge was the most helpful for me this week. I enjoyed having
+// practice building objects and manipulating them with functions. I also learned a lot about the process
+// of writing pseudocode and refactoring.
 // 
 // 
 // 
